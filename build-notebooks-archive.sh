cdc1ed424c6f1b6fdea09d9d92708846b1b83cc4
Richard (Rich) Vuduc <richie@cc.gatech.edu>
2019-07-17 15:17:41 -0400

    Added a sparse matrix/graph example: triangle counting

--- build-notebooks-archive.sh
+++ build-notebooks-archive.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git ls-tree --full-tree -r --name-only HEAD \
-    | grep "^notebooks" \
+    | grep "^notebooks/" \
     | xargs tar cvf - \
     | bzip2 -c > notebooks.tar.bz2
 
